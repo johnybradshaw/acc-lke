3152e4442ae581d7b5e5716fafd2cd4ee09c58a8
John <johnybradshaw@users.noreply.github.com>
2024-01-09 11:44:35 +0000

    Add cluster name suffix

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 # main.tf
 
 # This file is used to create a Linode Kubernetes Engine (LKE) cluster.
+resource "random_string" "cluster_suffix" {
+  length = 4
+  special = false
+  upper = false
+  numeric = false
+}
 
 // Create the LKE cluster
 resource "linode_lke_cluster" "lke_cluster" {
   provider = linode.default # Use the default Linode provider
 
-  label       = var.lke_cluster.name
+  label       = "${var.lke_cluster.name}-${random_string.cluster_suffix.result}"
   region      = var.lke_cluster.region
   k8s_version = var.lke_cluster.k8s_version
   tags        = var.lke_cluster.tags
--- provider.tf
+++ provider.tf
@@ -7,12 +7,23 @@ terraform {
         linode = {
             source = "linode/linode"
             version = ">= 2.9.3"
-            configuration_aliases = [ linode.default ]
+            #configuration_aliases = [ linode.default ]
+        }
+        random = {
+            source = "hashicorp/random"
+            version = ">= 2.3.0"
+            #configuration_aliases = [ random.default ]
         }
     }
 }
 
 provider "linode" {
     alias = "default"
+    
     token = var.linode_config.api_token
+}
+
+# Initialise the Random provider
+provider "random" {
+    # alias = "default"
 }
